dc620f76ce89d66749c6d2a9c836529b4cb38527
kavonraggs <kavonraggs@gmail.com>
2025-10-22 12:03:46 -0500

    methods created

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,41 @@
 package com.pluralsight;
 
 public class UserInterface {
+    Dealership dealership;
+
+    public UserInterface(){
+
+    }
+
+    public void display(){
+
+    }
+
+    public void processGetByPriceRequest(){
+
+    }
+    public void processGetByMakeModelRequest(){
+
+    }
+    public void processGetByYearRequest(){
+
+    }
+    public void processGetByColorRequest(){
+
+    }
+    public void processGetByMileageRequest(){
+
+    }
+    public void processGetByVehicleTypeRequest(){
+
+    }
+    public void processGetByAllVehicleRequest(){
+
+    }
+    public void processAddVehicle(){
+
+    }
+    public void processRemoveVehicle(){
+
+    }
 }
